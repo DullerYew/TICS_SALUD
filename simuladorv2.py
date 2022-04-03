84e4a224d3d43b7c664ab57b6673ca0b1a357bab
NictedelRayo <nicteov1912@gmail.com>
2022-04-02 22:14:35 -0500

    Ingresa a vitales, falta meter datos reales

--- simuladorv2.py
+++ simuladorv2.py
@@ -34,34 +34,115 @@ username_text_field.send_keys(Keys.ENTER)
 patient_menu = driver.find_element_by_xpath("//*[@id='mainMenu']/div/div[6]/div/div")   #Acceso al menu de "Patient"
 patient_menu.click()
 
-time.sleep(5) # Let the user actually see something!
-patient_menu_new_search = driver.find_element_by_xpath("//*[@id='mainMenu']/div/div[6]/div/ul/li[1]") #Acceso al sub menu New/search para registrar un nuevo paciente
+time.sleep(1) # Let the user actually see something!
+patient_menu_new_search = driver.find_element_by_xpath("//*[@id='mainMenu']/div/div[6]/div/ul/li[1]/div") #Acceso al sub menu New/search para registrar un nuevo paciente
 patient_menu_new_search.click()
 
+#Cambia de frame a Nuevo Paciente
+frame_0 = driver.find_element_by_xpath("//*[@id='framesDisplay']/div[3]/iframe")
+driver.switch_to.frame(frame_0)
 
+#INGRESAR VARIABLE NOMBRE
+name = driver.find_element_by_name('form_fname')
+name.send_keys('Alexis')
+time.sleep(1) 
 
+#INGRESAR VARIABLE APELLIDO
+apellido = driver.find_element_by_name('form_lname')
+apellido.send_keys('Ortega')
+time.sleep(1) 
 
+#INGRESAR VARIABLE RFC
+rfc = driver.find_element_by_name('form_pubpid')
+rfc.send_keys('323232')
+time.sleep(1)
 
+#INGRESAR VARIABLE SEXO, CAMBIAR POR MALE O FEMALE
+sexo = driver.find_element_by_name('form_sex')
+sexo.send_keys('Male')
+time.sleep(1) 
 
+#INGRESAR VARIABLE FECHA DE NACIMIENTO FORMATO DIA, MES AÑO
+fecha = driver.find_element_by_name('form_DOB')
+fecha.send_keys('1997-11-19')
+time.sleep(1) 
 
+#Crear nuevo paciente
+boton = driver.find_element_by_name('create')
+boton.click()
+time.sleep(1)
 
-time.sleep(5)
-#main_menu = driver.find_element_by_id("mainMenu")
-#list_elements = main_menu.find_elements_by_tag_name("div")
-#menu_patient = 
-#login_form = driver.find_element_by_xpath("//*[@id='tabs_div']/div/div[4]").is_displayed()
-#login_form.click()
+#Vuelve al frame original e ingresa al popup
+driver.switch_to.default_content()
+frame_1 = driver.find_element_by_id('modalframe')
+driver.switch_to.frame(frame_1)
 
+#Confirma el nuevo paciente
+boton1 = driver.find_element_by_xpath("//*[@id='searchResultsHeader']/center/input")
+boton1.click()
+time.sleep(1) 
 
+#Espera a que aparezca la alerta y la acepta
+WebDriverWait(driver, 10).until(EC.alert_is_present())
+driver.switch_to.alert.accept()
 
+#Regresa al frame principal y selecciona paciente de nuevo
+driver.switch_to.default_content()
+el = driver.find_element_by_xpath("//*[@id='mainMenu']/div/div[6]/div/div")
+el.click()
+time.sleep(1) 
 
+#Selecciona Visits
+el = driver.find_element_by_xpath("//*[@id='mainMenu']/div/div[6]/div/ul/li[3]/div/div")
+el.click()
+time.sleep(1) 
 
-#element = WebDriverWait(driver,15).until(EC.element_to_be_clickable((By.XPATH,"/html/body/div/nav/div/div/div[6]/div/ul/li[1]")))
-#element.click()
+#Selecciona Create Visit
+el = driver.find_element_by_xpath("//*[@id='mainMenu']/div/div[6]/div/ul/li[3]/div/ul/li[1]/div")
+el.click()
+time.sleep(1) 
 
-#print(list_elements)
+#Ingresa al frame de Create Visit
+frame = driver.find_element_by_xpath("//*[@id='framesDisplay']/div[4]/iframe")
+driver.switch_to.frame(frame)
 
+#Selecciona Categoria Nuevo paciente
+new = driver.find_element_by_name('pc_catid')
+new.send_keys('New Patient')
+time.sleep(1) 
 
-#driver.close()   #Cierra la pestaña actual 
+#Guarda la visita
+el = driver.find_element_by_xpath("//*[@id='new-encounter-form']/div/div/div/button[1]")
+el.click()
+time.sleep(1)
 
-driver.quit()  #Cierra el navegador 
+#Cambia a el nuevo path
+frame = driver.find_element_by_xpath("//*[@id='enctabs-1']/iframe")
+driver.switch_to.frame(frame)
+
+#Selecciona Categoria Clinical
+el = driver.find_element_by_xpath("//*[@id='category_Clinical']")
+el.click()
+time.sleep(1) 
+
+#Selecciona Vitals
+el = driver.find_element_by_xpath("//*[@id='navbarSupportedContent']/ul[1]/li[2]/div/a[11]")
+el.click()
+time.sleep(1) 
+
+#Se cambia al frame de vitals
+driver.switch_to.default_content()
+frame_1 = driver.find_element_by_xpath("//*[@id='framesDisplay']/div[4]/iframe")
+driver.switch_to.frame(frame_1)
+frame_1 = driver.find_element_by_xpath("//*[@id='enctabs-1001']/iframe")
+driver.switch_to.frame(frame_1)
+
+#Escribe el campo de peso INGRESAR LA VARIABLE PESO
+peso = driver.find_element_by_xpath("//*[@id='weight_input_metric']")
+peso.send_keys('80')
+time.sleep(1)
+
+#Escribir los demas vitales
+
+driver.quit()
+
